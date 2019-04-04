c5d52cbab2e77633966ac0d3d9147b41f88766e7
Will Blanton <will@Wills-MacBook-Pro.local>
2019-04-04 17:53:49 -0400

    get_shortlink()

--- src/Constants.hx
+++ src/Constants.hx
@@ -1,7 +1,7 @@
 class Constants
 {
 
-	public static var VERSION:String = '0.1.7';
+	public static var VERSION:String = '0.1.8';
 	public static var FLIP_TIME:Float = 0.12;
 	public static var CARD_W:Float = 8;
 	public static var CARD_H:Float = 12;
--- src/URLHandler.hx
+++ src/URLHandler.hx
@@ -1,3 +1,4 @@
+import haxe.Json;
 using Math;
 using StringTools;
 
@@ -5,22 +6,24 @@ using StringTools;
 class URLHandler
 {
 
+	static var card_separator:String = '+';
+	static var value_separator:String = '|';
+	static var suits:Array<String> = ['hearts', 'diamonds', 'spades', 'clubs', 'special'];
+
 	public static function check_url()
 	{
 		var url = js.Browser.window.location.href;
-		trace('URL:', url);
 		var split_url = url.split('?');
 		if (split_url.length != 2) return;
-		trace('Dynamic:', split_url[1]);
 		var cards_data = split_url[1].urlDecode().split('+');
 		for (data in cards_data) if (!validate_card(data)) return;
-		for (data in cards_data) if (data.length > 0) add_card(data.split('/'));
+		for (data in cards_data) if (data.length > 0) add_card(data.split(value_separator));
 	}
 
 	static function validate_card(card:String):Bool
 	{
 		if (card.length == 0) return true;
-		var data = card.split('/');
+		var data = card.split(value_separator);
 		if (data.length != 4) return false;
 		for (i in 0...3) if (Math.isNaN(Std.parseInt(data[i]))) return false;
 		if (Std.parseInt(data[3]) < 0 || Std.parseInt(data[3]) > 4) return false;
@@ -34,7 +37,7 @@ class URLHandler
 		var suit = Std.parseInt(data[2]);
 		var text = data[3];
 		var card = new Card(x, y);
-		card.set_data({ text: text, suit: ['hearts', 'diamonds', 'spades', 'clubs', 'special'][suit] });
+		card.set_data({ text: text, suit: suits[suit] });
 		card.locked = true;
 	}
 
@@ -46,9 +49,27 @@ class URLHandler
 		{
 			var card:Card = cast card;
 			var data = card.get_data();
-			str += '+${(card.x / GRID_W).round()}/${(card.y / GRID_H).round()}/${['hearts', 'diamonds', 'spades', 'clubs', 'special'].indexOf(data.suit)}/${data.text}';
+			str += '$card_separator${(card.x / GRID_W).round()}$value_separator${(card.y / GRID_H).round()}$value_separator${suits.indexOf(data.suit)}$value_separator${data.text}';
 		}
 		return url += str.urlEncode();
+	}
+
+	public static function get_shortlink()
+	{
+		var request = new haxe.Http('https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyClYWNI-sA5nXkayNwTx72uL44ZYTMg9X4');
+		var data = Json.stringify({
+			dynamicLinkInfo: {
+				domainUriPrefix: "https://cardlink.page.link",
+				link: get_url()
+			},
+			suffix:{
+				option:"SHORT"
+			}
+		});
+		request.setPostData(data);
+		request.onData = (e) -> trace(e);
+		request.onError = (e) -> trace(e);
+		request.request(true);
 	}
 
 }
